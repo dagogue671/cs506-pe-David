44dfaf592864903fd8ea98c1da7ab208abebc328
David Gogue <dagogue671@gmail.com>
2025-04-13 21:55:14 +1000

    Update Repository

---
+++ PE01/Problem1.py
@@ -0,0 +1,34 @@
+"""
+Problem 1: Reduce to Zero
+Given a non-negtive integer num, return the number of steps to reduceit to zero
+If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1
+from it.
+"""
+
+step_counter = 0 # Count the number of steps/iterations
+
+non_negative_number = input('Please Enter a Non-Negative Integer: ')
+non_negative_number = int(non_negative_number)
+
+# Check if input is positive or negative
+if non_negative_number <= 0:
+    while non_negative_number <= 0:
+        non_negative_number = input('Please Enter a Non-Negative Integer: ')
+        non_negative_number = int(non_negative_number)
+
+
+while non_negative_number != 0:
+    prev_num = non_negative_number # place holder for the non_negative_number
+    # Divide by 2 if the non_negative_number is even
+    if non_negative_number % 2 == 0:
+        non_negative_number /= 2
+        non_negative_number = int(non_negative_number)
+        print(f"{prev_num} is even; divide by 2 and obtain {non_negative_number}")
+        step_counter += 1
+    # Subtract by 1 if the non_negative_number is odd
+    else:
+        non_negative_number -= 1
+        print(f"{prev_num} is odd; subtract 1 and obtain {non_negative_number}")
+        step_counter += 1
+
+print(f"Number of Steps taken is {step_counter}")
---
+++ PE01/Problem2.py
@@ -0,0 +1,15 @@
+"""
+Problem 2: Add Digits
+Given a non-negative integer num, repeatedly add all its digits until the result has only one digit
+
+Example:
+Input: 38 Output:2
+3 + 8 = 11 => 1 + 1 = 2
+
+Follow up: 
+Could you do it without any loop/recursion?
+"""
+
+
+non_negative_num = input("Please enter a non-negative number: ") # Input saved as a string
+
